3659e17e12be0e07c47e1f451f358758a4926669
Beginner10617 <wasihusain23@iitk.ac.in>
2026-05-11 11:31:08 +0530

    Added example-2

--- main.c
+++ main.c
@@ -17,26 +17,14 @@ int main() {
   Value *X4 = doubleToValue(0.8f, true);
   Value *X5 = doubleToValue(0.2, true);
   Value *X6 = doubleToValue(1.2, true);
-  Value *X7 = doubleToValue(-0.6, true);
-  Value *X8 = doubleToValue(0.7, true);
-  Value *X9 = doubleToValue(-0.9, true);
   Value *Y1 = EmptyValue(false);
   Value *Y2 = EmptyValue(false);
   Value *Y3 = EmptyValue(false);
   Value *Z = EmptyValue(false);
 
-  setSum(Y1, 3);
-  addToSum(Y1, X1);
-  addToSum(Y1, X2);
-  addToSum(Y1, X3);
-  setSum(Y2, 3);
-  addToSum(Y2, X4);
-  addToSum(Y2, X5);
-  addToSum(Y2, X6);
-  setSum(Y3, 3);
-  addToSum(Y3, X7);
-  addToSum(Y3, X8);
-  addToSum(Y3, X9);
+  setMul(Y1, X1, X2);
+  setMul(Y2, X3, X4);
+  setMul(Y3, X5, X6);
   setSum(Z, 3);
   addToSum(Z, Y1);
   addToSum(Z, Y2);
@@ -57,15 +45,12 @@ int main() {
 
   printValue(X1);
   printValue(X2);
-  printValue(X3);
   printf("\n");
+  printValue(X3);
   printValue(X4);
+  printf("\n");
   printValue(X5);
   printValue(X6);
-  printf("\n");
-  printValue(X7);
-  printValue(X8);
-  printValue(X9);
   printf("\n");
   printValue(Y1);
   printValue(Y2);
