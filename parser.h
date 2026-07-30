8b3c440133d93ea1eb8e85d54e3d2c45a034ead4
Beginner10617 <wasihusain23@iitk.ac.in>
2026-07-30 10:50:15 +0530

    Added declarations for file handling

--- parser.h
+++ parser.h
@@ -1,21 +1,22 @@
 #ifndef PARSER_H
 #define PARSER_H
-
-#include "stdint.h"
-#include "stdio.h"
-
+#include "autograd.h"
+#include <stdint.h>
+#include <stdio.h>
 // Converts little-endian to big-endian
 uint32_t swap(uint32_t x);
-
 // calls swap() and replaces the output in-place
 void swapInPlace(uint32_t *arr, int size);
-
 // Reads and display head of image dataset
 void readImgHeader(uint32_t *head, FILE *ptr);
-
 // Reads and display head of label dataset
 void readLabelHeader(uint32_t *head, FILE *ptr);
-
 // Reads next image int uint8_t *img
-int readNextImage(uint8_t *img, int img_size, FILE *ptr); 
+int readNextImage(uint8_t *img, int img_size, FILE *ptr);
+// Save MLP parameters in a textfile
+void saveMLP(MLP *mlp, const char *Fname);
+// Load MLP from parameters specified in a textfile
+MLP *loadMLP(const char *Fname);
+// validate the file before parsing, non zero value in case of error
+int validate(const char *Fname);
 #endif
